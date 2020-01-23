f302a40bf76783667d04530f12047328b645e676
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2020-01-23 14:59:51 -0800

    project completed without styling yet

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-search-bar',
-  templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  selector: "app-search-bar",
+  templateUrl: "./search-bar.component.html",
+  styleUrls: ["./search-bar.component.css"]
 })
 export class SearchBarComponent implements OnInit {
+  @Output() submitted = new EventEmitter<string>();
 
-  constructor() { }
+  term = "";
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onFormSubmit($event: any) {
+    $event.preventDefault();
+    this.submitted.emit(this.term);
   }
-
 }
